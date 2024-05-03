55aaccddf19a24b8a6dcb32af7dfbbb6673b9b6d
U-UNIMES\bquiller <bquiller@DSI-5TCH7D3.unimes.local>
2024-05-03 09:58:03 +0200

    Ajout des WS carte et profil (avec les fichiers !)

---
+++ src/Controller/CarteController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use L3\Bundle\LdapUserBundle\Entity\LdapUser;
+use OpenLdapObject\Builder\Condition;
+use Symfony\Component\Ldap\Ldap;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\{JsonResponse, Response,Request};
+
+class CarteController extends AbstractController
+{
+    #[Route('/carte/{username}', name: 'carte', methods: ['GET'])]
+    public function carte(Request $request, string $username): JsonResponse
+    {
+        $ldap = Ldap::create('ext_ldap', ['connection_string' => 'ldap://'.$this->getParameter('ldap_hostname').':389']);
+        $query = $ldap->query('ou=people,'.$this->getParameter('ldap_base_dn'), '(&(uid='.$username.'))');
+        $ldap->bind($this->getParameter('ldap_dn'), $this->getParameter('ldap_password'));
+        $results = $query->execute();
+        
+        foreach( $results as $index => $tab ) {
+            //print_r($tab);exit;
+            $nom = $tab->getAttribute('sn')[0];
+            $prenom = $tab->getAttribute('givenName')[0];
+            $naissance = $tab->getAttribute('dateNaissance')[0];
+            $genre = $tab->getAttribute('supannCivilite')[0];
+            $affiliation = $tab->getAttribute('eduPersonPrimaryAffiliation')[0];
+            $photo = 'data:image/jpeg;base64,'.base64_encode($tab->getAttribute('jpegPhoto')[0]);
+            $csn = $tab->getAttribute('unimesCarte')[0];
+            if ($tab->getAttribute('supannCodeINE') !== null) {
+                $title="Carte professionnelle";
+                $identifiant = $tab->getAttribute('supannCodeINE')[0];
+            } else if ($tab->getAttribute('supannEmpId') !== null) {
+                $title="Carte étudiant";
+                $identifiant = $tab->getAttribute('supannEmpId')[0];
+            }
+            if (date("m") > 8) $fin = date("Y")+1;
+            else $fin = date("Y");
+        }
+
+        $data = array(
+          "lastname"=> $nom,
+          "firstname"=> $prenom,
+          "birthdate"=> $naissance,
+          "gender"=> $genre,
+          "affiliation"=> $affiliation,
+          "photo"=> $photo,
+          "ine"=> $identifiant,
+          "errors"=>array(), 
+          "cards"=>array(
+              "euStudentCard"=> array(
+                "title"=>$title,
+                "subtitle"=>null,
+                "endDate"=>$fin,
+                "idNumber"=>$csn,
+                "csn"=>$csn,
+                "qrCode"=>array(
+                    "type"=>"text",
+                    "value"=>$username.'@unimes.fr')
+                ), 
+            ) 
+        );
+
+        return $this->json($data);
+
+    }
+
+}
---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use L3\Bundle\LdapUserBundle\Entity\LdapUser;
+use OpenLdapObject\Builder\Condition;
+use Symfony\Component\Ldap\Ldap;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\{JsonResponse, Response,Request};
+
+class ProfilController extends AbstractController
+{
+    #[Route('/profil/{username}', name: 'profil', methods: ['GET'])]
+    public function profil(Request $request, string $username): JsonResponse
+    {
+        // $user = $ldapUP->loadUserByIdentifier($username);
+        $ldap = Ldap::create('ext_ldap', ['connection_string' => 'ldap://'.$this->getParameter('ldap_hostname').':389']);
+        $query = $ldap->query('ou=people,'.$this->getParameter('ldap_base_dn'), '(&(uid='.$username.'))');
+        $ldap->bind($this->getParameter('ldap_dn'), $this->getParameter('ldap_password'));
+        $results = $query->execute();
+        
+        foreach( $results as $index => $tab ) {
+            //print_r($tab);exit;
+            $nom = $tab->getAttribute('sn')[0];
+            $prenom = $tab->getAttribute('givenName')[0];
+            $nomPrenom = $tab->getAttribute('displayName')[0];
+            $mail = $tab->getAttribute('mail')[0];
+            $affiliation = $tab->getAttribute('eduPersonPrimaryAffiliation')[0];
+        }
+
+        $data = array(
+          "displayName"=> $nomPrenom,
+          "name"=> $nom,
+          "firstname"=> $prenom,
+          "email"=> $mail,
+          "roles"=> $affiliation
+        );
+
+        return $this->json($data);
+
+    }
+
+}
